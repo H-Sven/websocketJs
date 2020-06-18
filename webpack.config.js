d5c92fac434165188c72337df5658cb20a366689
Siwen <900345@qq.com>
2020-06-18 11:50:14 +0800

    es6

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: Siwen
  * @Date: 2020-06-15 14:59:42
  * @LastEditors: Siwen
- * @LastEditTime: 2020-06-17 14:23:10
+ * @LastEditTime: 2020-06-18 11:49:23
  * @Description: webpack.config.js
  */
 const webpack = require('webpack')
@@ -14,8 +14,19 @@ module.exports = {
     index: './index.js'
   },
   output: {
+    library: "ws",
+    libraryTarget: "umd",
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin()
